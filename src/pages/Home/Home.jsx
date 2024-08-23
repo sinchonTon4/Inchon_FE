f30eb31999e1352c19706b49146e0cf66fff1c68
yoominwoo <mwlucas@naver.com>
2024-08-24 01:51:07 +0900

    중간 저장

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,93 @@
 import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <SectionWrapper>
+      <div className="sectionWrapper">
+        <div className="tagWrapper">
+          <div className="titleTag">인기 공동 구매</div>
+          <div className="subTagWrapper">
+            <button className="subTag">식료품</button>
+            <button className="subTag">생필품</button>
+          </div>
+        </div>
+        <div className="contentWrapper">
+          <div className="shareSecond">2</div>
+          <div className="shareFirst">1</div>
+          <div className="shareThird">3</div>
+        </div>
+      </div>
+      <div className="sectionWrapper">
+        <div className="tagWrapper">
+          <div className="titleTag">커뮤니티 인기글</div>
+          <div className="subTagWrapper">
+            <button className="subTag">전체</button>
+            <button className="subTag">요리</button>
+            <button className="subTag">생활</button>
+          </div>
+        </div>
+        <div className="contentWrapper">
+          <div className="shareSecond">2</div>
+          <div className="shareFirst">1</div>
+          <div className="shareThird">3</div>
+        </div>
+      </div>
+    </SectionWrapper>
+  );
 };
 
 export default Home;
 
+const SectionWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 50px;
 
+  .sectionWrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 90%;
+  }
+
+  .tagWrapper {
+    display: flex;
+  }
+
+  .titleTag {
+    width: 25%;
+    background: #53acff;
+    color: white;
+    text-align: center;
+    font-family: "SCD-Medium";
+    font-size: 35px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    /* padding: 16px 25px 16px 25px; */
+    width: 320px;
+    height: 74px;
+  }
+
+  .subTag {
+    width: 10%;
+    background: #53acff;
+    color: white;
+    text-align: center;
+    font-family: "SCD-Medium";
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    padding: 16px 25px 16px 25px;
+  }
+
+  .contentWrapper {
+    background-color: #f6f6f6;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    height: 60vh;
+  }
+`;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const Login = () => {
   const [input, setInput] = useState({
@@ -56,9 +57,13 @@ const Login = () => {
   };
 
   return (
-    <div>
-      .title
-      <div>유니빌</div>
+    <LoginSection>
+      <div className="titleSection">
+        <MainText className="mainText">유니빌</MainText>
+        <SubText className="subText">
+          자취생의 절약을 위한 공동 구매 커뮤니티{" "}
+        </SubText>
+      </div>
       <div className="inputSection">
         <input
           type="text"
@@ -75,11 +80,98 @@ const Login = () => {
           onChange={onChange}
         />
       </div>
-      <button className="loginBtn" onClick={onSubmit}>
-        로그인
-      </button>
-    </div>
+      <div className="buttonSection">
+        <button className="loginBtn" onClick={onSubmit}>
+          로그인
+        </button>
+        <button
+          className="loginBtn"
+          onClick={() => {
+            nav("/signUp");
+          }}
+        >
+          회원가입
+        </button>
+      </div>
+    </LoginSection>
   );
 };
 
 export default Login;
+
+const LoginSection = styled.div`
+  * {
+    box-sizing: border-box;
+  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+  margin: 0 auto;
+  margin-top: 10vh;
+  gap: 40px;
+
+  .titleSection {
+    text-align: center;
+  }
+
+  .inputSection {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 100%;
+  }
+
+  .inputSection input {
+    width: 100%;
+    border: 5px solid #c0e1ff;
+    padding: 15px 40px;
+    color: #53acff;
+    font-size: 20px;
+    font-family: "SCD-Medium";
+    &:focus {
+      outline: none;
+    }
+    &::placeholder {
+      color: #53acff;
+      font-size: 20px;
+    }
+  }
+
+  .buttonSection {
+    display: flex;
+    width: 90%;
+    justify-content: space-between;
+  }
+
+  .buttonSection button {
+    background-color: #53acff;
+    font-family: "SCD-Medium";
+    border: none;
+    color: white;
+    font-size: 24px;
+    width: 233px;
+    height: 60px;
+    cursor: pointer;
+  }
+`;
+
+const MainText = styled.div`
+  color: #53acff;
+  text-align: center;
+  font-family: "SSanTokki";
+  font-size: 100px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin-bottom: 20px;
+`;
+
+const SubText = styled.div`
+  color: #94cbff;
+  font-family: "SSanTokki";
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
