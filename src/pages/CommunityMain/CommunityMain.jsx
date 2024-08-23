52e1380578308c8756dd3f0c08ff82f37b063905
Cuyqo <saon0410@gmail.com>
2024-08-24 01:41:45 +0900

    [Design] Header 구현

--- src/pages/CommunityMain/CommunityMain.jsx
+++ src/pages/CommunityMain/CommunityMain.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { instance } from "../../api/instance";
 import styled from "styled-components";
+import Headers from "../../components/Headers.jsx";
 
 // const LIMIT = 6; 몇개씩 줄거냐
 
@@ -60,11 +61,16 @@ const CommunityMain = () => {
 
   return (
     <div>
-      CommunityMain
-      <select value={order} onChange={setOrderCategory}>
+      <Headers />
+      {/* <select value={order} onChange={setOrderCategory}>
         <option value="최신순">최신순</option>
         <option value="공감순">공감순</option>
-      </select>
+      </select> */}
+      <ButtonWrap>
+        <CategoryButton>전체</CategoryButton>
+        <CategoryButton>요리</CategoryButton>
+        <CategoryButton>생활</CategoryButton>
+      </ButtonWrap>
       <select value={category} onChange={setCategoryDetail}>
         <option value="식재료">식재료</option>
         <option value="생필품">생필품</option>
@@ -87,4 +93,27 @@ export default CommunityMain;
 
 const MyDiv = styled.div`
   font-family: "SSanTokki";
+`;
+
+const CategoryButton = styled.button`
+  width: 319px;
+  height: 78px;
+  flex-shrink: 0;
+  background: #53acff;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.3);
+  border: none;
+  color: #fff;
+  text-align: center;
+  font-family: "SCD_Medium";
+  font-size: 35px;
+  font-style: normal;
+  line-height: normal;
+`;
+
+const ButtonWrap = styled.div`
+  width: calc(100%-335px);
+  margin: 47px 167.5px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
