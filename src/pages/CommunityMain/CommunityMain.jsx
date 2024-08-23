f293d0064a989e66447c9f5124d4f08fb4b08050
Cuyqo <saon0410@gmail.com>
2024-08-24 00:44:46 +0900

    [Design] 폰트 추가

--- src/pages/CommunityMain/CommunityMain.jsx
+++ src/pages/CommunityMain/CommunityMain.jsx
@@ -1,11 +1,90 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { instance } from "../../api/instance";
+import styled from "styled-components";
+
+// const LIMIT = 6; 몇개씩 줄거냐
+
+//최신순
+// 따로 get  공감순 필터링
+
+const CommunityMainItem = ({ item }) => {
+  return (
+    <div>
+      {item.content}
+      {/* <Link to="/comunityDeatil/:community_id"></Link> */}
+    </div>
+  );
+};
 
 const CommunityMain = () => {
-    return (
-        <div>
-            CommunityMain
-        </div>
-    );
+  const [order, setOrder] = useState("최신순");
+  const [category, setCategory] = useState("식재료");
+  const [items, setItems] = useState([
+    { content: "더미데이터" },
+    { content: "덤데2" },
+    { content: "zzzzz2" },
+  ]);
+  const [offeset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
+
+  const setOrderCategory = (event) => {
+    setOrder(event.target.value);
+    console.log(order);
+  };
+
+  const setCategoryDetail = (event) => {
+    setOrder(event.target.value);
+    console.log(order);
+  };
+
+  // 처음에 들어오자마자 가져오는 데이터는 최신순  공감순 누르면 공감순 정렬 데이터
+  // 만약 nextCursor가 null이면 버튼 비활성화
+  // const handleLoad = async (options) => {
+  //   try {
+  //     const res = await instance.get(`/search?order=${options.order}&offset=${options.offset}&limit=${options.limit}`);
+  //     setItems(res.data.items);
+  //     setHasNext(res.data.hasNext); // 예시로 hasNext를 응답 데이터에서 가져옴
+  //   } catch (e) {
+  //     console.log(e.message);
+  //   }
+  // };
+
+  // const handleLoadMore = async () => {
+  //   await handleLoad({ order, offset, limit: 6 }); // 예시로 LIMIT을 6으로 설정
+  //   setOffset((prev) => prev + 6); // 페이지네이션을 위해 오프셋 증가
+  // };
+
+  // useEffect(() => {
+  //   handleLoad({ order, offset: 0, limit: LIMIT });
+  // }, [order]);
+
+  return (
+    <div>
+      CommunityMain
+      <select value={order} onChange={setOrderCategory}>
+        <option value="최신순">최신순</option>
+        <option value="공감순">공감순</option>
+      </select>
+      <select value={category} onChange={setCategoryDetail}>
+        <option value="식재료">식재료</option>
+        <option value="생필품">생필품</option>
+      </select>
+      <MyDiv> Selected Order: {order}</MyDiv>
+      <div> Selected Categoty: {category}</div>
+      {items.length > 0 ? (
+        items.map((item, index) => (
+          <CommunityMainItem key={index} item={item} />
+        ))
+      ) : (
+        <div>작성된 글이 없습니다.</div>
+      )}
+      {hasNext && <button onClick={handleLoadMore}>더 보기</button>}
+    </div>
+  );
 };
 
 export default CommunityMain;
+
+const MyDiv = styled.div`
+  font-family: "SCD-Thin";
+`;
--- src/pages/CommunityNew/CommunityNew.jsx
+++ src/pages/CommunityNew/CommunityNew.jsx
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import FileInput from "./FileInput";
+import { instance } from "../../api/instance";
+
+// 사용자 사진 폼
+const WriteForm = () => {
+  const [userValues, setUserValues] = useState({
+    title: "",
+    tag: "",
+    content: "",
+    imgFile: null,
+  });
+
+  const [postData, setPostData] = useState({
+    title: "",
+    tag: "",
+    content: "",
+    imgFile: null,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //서버에 폼 보내고 성공하면 처리
+    //
+    // const postUserData = async () => {
+    //   try {
+    //     const res = await instance.post(`/url`, userValues);
+    //   } catch (e) {
+    //     console.log(e.messege);
+    //   }
+    // };
+
+    // 폼 제출 처리 로직 추가
+    console.log(userValues);
+    setUserValues({
+      title: "",
+      tag: "",
+      content: "",
+      imgFile: null,
+    });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleFileChange = (name, file) => {
+    setUserValues((prevValues) => ({
+      ...prevValues,
+      [name]: file,
+    }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="title"
+        type="text"
+        value={userValues.title}
+        onChange={handleChange}
+      />
+      <input
+        name="tag"
+        type="text"
+        value={userValues.tag}
+        onChange={handleChange}
+      />
+      <input
+        name="content"
+        type="text"
+        value={userValues.content}
+        onChange={handleChange}
+      />
+      <FileInput
+        name="imgFile"
+        value={userValues.imgFile}
+        onChange={handleFileChange}
+      />
+      <button type="submit">올리기</button>
+    </form>
+  );
+};
 
 const CommunityNew = () => {
-  return <div>CommunityNew</div>;
+  return (
+    <div>
+      CommunityNew
+      <WriteForm />
+    </div>
+  );
 };
 
 export default CommunityNew;
